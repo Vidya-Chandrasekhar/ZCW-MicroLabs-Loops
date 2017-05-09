ccd41cdb10d6480bfe70a61c3163e797542a0efd
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-05-09 12:16:38 -0400

    >oneToTen >oddNumbers >squares

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Numbers.java
@@ -3,17 +3,50 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Numbers {
 
+
+
+
     public String oneToTen(){
+
+        System.out.print(  "oneToTen()\n");
+        System.out.print(  " *** Output ***\n");
+
+        for (int i=1; i<11; i++){
+
+            System.out.println( i);
+        }
+
         return "";
     }
 
-    public String oddNumbers(){
+    public String oddNumbers() {
+
+        System.out.print("\noddNumbers()\n");
+        System.out.print(" *** Output ***\n");
+
+        for (int j = 1; j <= 20; j++) {
+            if (j % 2 == 1) {
+                System.out.println( j);
+            }
+        }
         return "";
     }
 
     public String squares(){
+
+        System.out.print("\nsquares()\n");
+        System.out.print(" *** Output ***\n");
+
+        int sqr;
+        int k = 1;
+        while (k <= 10) {
+            sqr  = (k*k);
+            k++;
+            System.out.println( sqr);
+        }
         return "";
     }
+
 
     public String random4(){
         return "";
