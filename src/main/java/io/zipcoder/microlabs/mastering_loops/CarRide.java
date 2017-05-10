698efa94122529688efa55320e5bfc181c060b3e
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-05-09 23:12:21 -0400

    "Are we there yet "CarRide lab

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,9 +1,20 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.Scanner;
+
 public class CarRide {
 
-    public String areWeThereYetTest(){
-        return "";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Are we there yet");
+            String userResponse = scanner.nextLine();
+            if (userResponse.equals("Yes")) {
+                System.out.println("Good");
+                break;
+            }
+        }
     }
 
 }
